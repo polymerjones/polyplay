2393e52bc249bbb58747a48672c1f712c81f9711
Paul Fisher <paul.t.fisher03@gmail.com>
2026-04-29 14:35:59 -0700

    Align bundle ID with submitted App Store build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.polyplay.app',
+  appId: 'com.paulfisher.polyplay',
   appName: 'PolyPlay',
   webDir: 'dist'
 };
